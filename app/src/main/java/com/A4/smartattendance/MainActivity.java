725f5f5b5eabd672901e948c74b2d5fb817afd85
MightyPhoenix <agnibesh00mukherjee@gmail.com>
2020-02-13 19:05:37 +0530

    first

---
+++ app/src/main/java/com/A4/smartattendance/MainActivity.java
@@ -0,0 +1,33 @@
+package com.A4.smartattendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user == null){
+            startActivity(new Intent(MainActivity.this,Dashboard.class));
+        }
+
+
+    }
+
+}
