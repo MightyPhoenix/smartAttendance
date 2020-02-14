e242abf529ddf9cc7061668e030a1a2ccf5975c4
Atiab <atiabkalam@gmail.com>
2020-02-14 13:04:44 +0530

    Login Backend - Atiab

--- app/src/main/java/com/A4/smartattendance/MainActivity.java
+++ app/src/main/java/com/A4/smartattendance/MainActivity.java
@@ -1,11 +1,16 @@
 package com.A4.smartattendance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -21,14 +26,92 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        mAuth.signInWithEmailAndPassword("atiabkalam@gmail.com","123456")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+//                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Login Success.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
 
-        if(user == null){
-            startActivity(new Intent(MainActivity.this,Dashboard.class));
-        }
-        else{
-            
-        }
+                        // ...
+                    }
+                });
+//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+//        if (user != null) {
+//            /*------------ If user's email is verified then access login -----------*/
+//            if(user.isEmailVerified()){
+//                Toast.makeText(MainActivity.this, "Login Success.",
+//                        Toast.LENGTH_SHORT).show();
+////                    startActivity(new Intent(MainActivity.this,Dashboard.class));
+////                    finish();
+//            }
+//            else {
+//                Toast.makeText(MainActivity.this, "Your Email is not verified.",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//        else{
+//            Toast.makeText(MainActivity.this, "No current user.",
+//                    Toast.LENGTH_SHORT).show();
+////            startActivity(new Intent(MainActivity.this,Dashboard.class));
+//        }
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    /*------------ If user's email is verified then access login -----------*/
+                    if(user.isEmailVerified()){
+                        Toast.makeText(MainActivity.this, "Login Success.",
+                                Toast.LENGTH_SHORT).show();
+//                    startActivity(new Intent(MainActivity.this,Dashboard.class));
+//                    finish();
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Your Email is not verified.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "No current user.",
+                            Toast.LENGTH_SHORT).show();
+//            startActivity(new Intent(MainActivity.this,Dashboard.class));
+                }
+
+            }
+        },5000);
+
+
+
+//        if(user == null){
+//            Toast.makeText(MainActivity.this, "No current user.",
+//                    Toast.LENGTH_SHORT).show();
+//            startActivity(new Intent(MainActivity.this,Dashboard.class));
+//            finish();
+//        }
+//        else{
+//            if(user.isEmailVerified()){
+//                Toast.makeText(MainActivity.this, "Login Success.",
+//                        Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(MainActivity.this,Dashboard.class));
+//            }
+//            else {
+//                Toast.makeText(MainActivity.this, "Your Email is not verified.",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        }
 
 
     }
